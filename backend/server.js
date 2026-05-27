b97745b4b070c1e299a1e09ee9a0f1c15da5c0a5
Giolianny <gioliannyt@icloud.com>
2026-05-28 00:34:46 +0200

    prueba server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("Servidor funcionando");
+  res.send("FUNCIONA RAILWAY");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor funcionando en puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en puerto", PORT);
 });
